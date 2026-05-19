ea3270f0785f06e7c79305a5d6667d6c2eea0edb
dalifyyy <d4lified@gmail.com>
2026-05-19 22:26:33 +0800

    .

---
+++ scripts/autoloads/dialogue_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var dialogue_box
+
+func say(text, to = dialogue_box):
+	pass
+
+func say_letter(letter):
+	pass
---
+++ scripts/autoloads/dialogues.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var dialogue: Dictionary = {
+	"name": {
+		"dialogue_1": ["line 1", "line 2"]
+	}
+}
